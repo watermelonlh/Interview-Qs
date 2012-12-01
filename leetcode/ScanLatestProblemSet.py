3e3a5f967eb9ed23c159f4d254f22b3dcc459558
watermelon <watermelonlh@gmail.com>
2012-12-01 13:33:06 +0800

    rename some error filename cpp, update scan script

--- leetcode/ScanLatestProblemSet.py
+++ leetcode/ScanLatestProblemSet.py
@@ -2,20 +2,49 @@
 
 import urllib2
 import HTMLParser
+import htmllib
 import re
+import os
+import sys
 
-website = urllib2.urlopen("http://leetcode.com/onlinejudge")
-website_html = website.read()
-parser = HTMLParser.HTMLParser()
-web = parser.unescape(website_html)
-print(web)
+class bcolors:
+    ROSE = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    PINK = '\033[91m'
+    ENDC = '\033[0m'
 
-#print website_html
+    @staticmethod
+    def printmsg(msg, color):
+        print color + msg + bcolors.ENDC
 
-problems = re.findall("setJudge\([0-9]+, \"([a-zA-Z0-9\(\),\- ]+)\"\)", web)
-print(problems)
 
-print("total=", len(problems))
+problems = []
+if len(sys.argv) > 1 and sys.argv[1] == '-fresh':
+    website = urllib2.urlopen("http://www.leetcode.com/onlinejudge")
+    website_html = website.read()
+    web = website_html.decode("utf-8")
+    parser = HTMLParser.HTMLParser()
+    web = parser.unescape(web)
+    problems = re.findall("setJudge\([0-9]+, \"([a-zA-Z0-9\(\),\'\- ]+)\"\)", web)
+
+    latestProblemFile = open("Latest Problem Set.txt", "w+")
+    for problem in problems:
+        latestProblemFile.write(problem + os.linesep)
+    latestProblemFile.close()
+
+else:
+    latestProblemFile = open("Latest Problem Set.txt", "r")
+    problems = latestProblemFile.read().splitlines()
+    latestProblemFile.close()
+
+bcolors.printmsg("Problem Amount = " + str(len(problems)), bcolors.PINK)
 for problem in problems:
-    print(problem)
+#    print(problem)
+    filename = problem.replace(' ', '.') + ".cpp"
+    if not os.path.exists(filename):
+        bcolors.printmsg(problem, bcolors.BLUE)
+
+
 
