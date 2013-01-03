ef97602533290b905789e58f2ad034f08ead5d4d
watermelon <watermelonlh@gmail.com>
2013-01-03 23:32:06 +0800

    implement in a simpler way

--- leetcode/Next.Permutation.cpp
+++ leetcode/Next.Permutation.cpp
@@ -4,27 +4,17 @@ public:
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
         int N = num.size();
-        for (int i = N - 1; i >= 0; i--)
+        int i = N - 1;
+        while (i > 0 && num[i - 1] >= num[i]) i--;
+        if (i > 0)
         {
-            if (!i || num[i - 1] < num[i])
-            {
-                if (i > 0)
-                {
-                    for (int j = N - 1; j >= i; j--)
-                    {
-                        if (num[j] > num[i - 1])
-                        {
-                            swap(num[j], num[i - 1]);
-                            break;
-                        }
-                    }
-                }
-                for (int l = i, r = N - 1; l < r; l++, r--)
-                {
-                    swap(num[l], num[r]);
-                }
-                break;
-            }
+            int j = N - 1;
+            while (num[j] <= num[i - 1]) j--;
+            swap(num[j], num[i - 1]);
+        }
+        for (int l = i, r = N - 1; l < r; l++, r--)
+        {
+            swap(num[l], num[r]);
         }
     }
 };
