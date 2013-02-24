7d6aa3b753ba1a1f78d176b66dd7ccbb4d3159a3
watermelon <watermelonlhsignup@gmail.com>
2013-02-24 19:48:21 +0800

    Update leetcode/Longest.Consecutive.Sequence.cpp

--- leetcode/Longest.Consecutive.Sequence.cpp
+++ leetcode/Longest.Consecutive.Sequence.cpp
@@ -1,39 +1,28 @@
 class Solution {
 public:
+    int getCount(unordered_set<int> &hash, int x, int step)
+    {
+        int count = 0;
+        while (hash.count(x))
+        {
+            count++;
+            hash.erase(x);
+            x += step;
+        }
+        return count;
+    }
     int longestConsecutive(vector<int> &num) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        unordered_map<int, int> hash;
+        unordered_set<int> hash;
         int N = num.size();
-        vector<bool> head(N, true);
-        vector<int> next(N, -1);
+        vector<int> count(N, 1);
         for (int i = 0; i < N; i++)
-            hash[num[i]] = i + 1;
+            hash.insert(num[i]);
             
-        for (int i = 0; i < N; i++)
-        {
-            if (hash[num[i] + 1] > 0)
-            {
-                next[i] = hash[num[i] + 1] - 1;
-                head[next[i]] = false;
-            }
-        }
-        
         int ans = 0;
         for (int i = 0; i < N; i++)
-        {
-            if (head[i])
-            {
-                int now = i;
-                int step = 0;
-                while (now >= 0)
-                {
-                    step++;
-                    now = next[now];
-                }
-                ans = max(ans, step);
-            }
-        }
+            ans = max(getCount(hash, num[i], -1) + getCount(hash, num[i] + 1, 1), ans);
         return ans;
     }
 };
