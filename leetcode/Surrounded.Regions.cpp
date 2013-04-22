765e0d80763231e0a4a84e1907f9a1c3b4e941f9
watermelon <watermelonlh@gmail.com>
2013-04-22 21:45:28 +0800

    Update Surrounded.Regions.cpp

--- leetcode/Surrounded.Regions.cpp
+++ leetcode/Surrounded.Regions.cpp
@@ -1,59 +1,52 @@
 class Solution {
 public:
-
     void solve(vector<vector<char>> &board) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        
         int N = board.size();
-        if (N == 0) return;
+        if (!N) return;
         int M = board[0].size();
-        if (M == 0) return;
-        vector<vector<bool> > mark(N, vector<bool>(M, false));
-        
+        if (!M) return;
         int dx[4] = {-1, 1, 0, 0};
         int dy[4] = {0, 0, -1, 1};
-        
         for (int i = 0; i < N; i++)
             for (int j = 0; j < M; j++)
-            {
-                if (!mark[i][j] && board[i][j] == 'O')
+            {   
+                if (board[i][j] == 'O')
                 {
-                    vector<int> queX, queY;
-                    queX.push_back(i);
-                    queY.push_back(j);
-                    mark[i][j] = true;
+                    vector<int> queX(0);
+                    vector<int> queY(0);
+                    queX.push_back(i), queY.push_back(j);
+                    board[i][j] = '.';
                     int head = 0;
-                    bool result = !(i == 0 || j == 0 || i == N - 1 || j == M - 1) ;
-                    
+                    bool edge = false;
                     while (head < queX.size())
                     {
-                        int nx = queX[head];
-                        int ny = queY[head];
+                        int x = queX[head], y = queY[head];
+                        if (!x || !y || x == N - 1 || y == M - 1) edge = true;
                         head++;
                         for (int i = 0; i < 4; i++)
                         {
-                            int nextX = nx + dx[i];
-                            int nextY = ny + dy[i];
-                            if (nextX >= 0 && nextX < N && nextY >= 0 && nextY < M)
-                            {
-                                if (!mark[nextX][nextY] && board[nextX][nextY] == 'O')
+                            int nx = x + dx[i], ny = y + dy[i];
+                            if (nx >= 0 && ny >= 0 && nx < N && ny < M)
+                                if (board[nx][ny] == 'O')
                                 {
-                                    mark[nextX][nextY] = true;
-                                    queX.push_back(nextX);
-                                    queY.push_back(nextY);
-                                    if (nextX == 0 || nextY == 0 || nextX == N - 1 || nextY == M - 1) 
-                                        result = false;
+                                    board[nx][ny] = '.';
+                                    queX.push_back(nx);
+                                    queY.push_back(ny);
                                 }
-                            }
                         }
                     }
-                    if (result)
+                    if (edge)
                     {
                         for (int i = 0; i < queX.size(); i++)
-                            board[queX[i]][queY[i]] = 'X';
+                            board[queX[i]][queY[i]] = ',';
                     }
                 }
             }
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < M; j++)
+                if (board[i][j] == ',') board[i][j] = 'O';
+                else board[i][j] = 'X';
     }
 };
