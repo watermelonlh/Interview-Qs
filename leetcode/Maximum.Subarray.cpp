9f11120252aa4206b99615c7bcf77ca71bf21929
watermelon <watermelonlh@gmail.com>
2013-05-06 20:27:27 +0800

    Update Maximum.Subarray.cpp

--- leetcode/Maximum.Subarray.cpp
+++ leetcode/Maximum.Subarray.cpp
@@ -3,21 +3,12 @@ public:
     int maxSubArray(int A[], int n) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        int ans = INT_MIN;
-        for (int i = 0; i < n; i++)
-            if (A[i] > ans) ans = A[i];
-        
-        int sum = 0;
+        int ans = A[0], sum = 0;
         for (int i = 0; i < n; i++)
         {
-            if (sum + A[i] <= 0)
-            {
-                sum = 0;
-            } else
-            {
-                sum += A[i];
-                ans = max(sum, ans);
-            }
+            sum += A[i];
+            ans = max(ans, sum);
+            if (sum < 0) sum = 0;
         }
         return ans;
     }
